07b758e64c5c9b3548dbe6db9b3a3a46d2e065eb
Lars <hancop@hotmail.de>
2017-02-15 11:50:01 +0100

    Added element vars

--- app/src/main/java/com/android/misterevo/datachanger/MainActivity.java
+++ app/src/main/java/com/android/misterevo/datachanger/MainActivity.java
@@ -2,12 +2,22 @@ package com.android.misterevo.datachanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Getting interface
+    Button b1;
+    TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
