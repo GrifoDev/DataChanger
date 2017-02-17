f310792443eefd8f6fc3304b8511b41b1d659002
Lars <hancop@hotmail.de>
2017-02-17 09:52:27 +0100

    Cleaned up some code, added comments and simplified the code

--- app/src/main/java/com/android/misterevo/datachanger/FileHelper.java
+++ app/src/main/java/com/android/misterevo/datachanger/FileHelper.java
@@ -1,8 +1,9 @@
 package com.android.misterevo.datachanger;
 
-import android.content.Context;
+
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 /**
@@ -18,49 +20,80 @@ import java.io.InputStreamReader;
  */
 
 public class FileHelper {
+
     final static String filename = "others.xml";
     final static String path = (Environment.getRootDirectory().getAbsolutePath() + "/csc/");
     final static String TAG = FileHelper.class.getName();
+    public static File xmlText = new File(path, filename);
 
-    public static String ReadFile ( Context context) {
-        String line = null;
-
+    public static String readFile () {
+        String content;
         try {
+            //Acquire root-access
             Process p;
             p = Runtime.getRuntime().exec("su");
 
-            FileInputStream fileInputStream = new FileInputStream(new File(path, filename));
+            //FileInputStream loads the xmlText- File
+            //InputStreamReader reads the bytes coming from the FIS
+            //BufferedReader is converting to text?? -- Needs confirmation
+            //StringBuilder puts the single lines to one whole String together
+
+            FileInputStream fileInputStream = new FileInputStream(xmlText);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder stringBuilder = new StringBuilder();
 
-            while ( (line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line + System.getProperty("line.separator"));
+            //Checking if there is a line to read
+            //Stringbuilder adds the line to the Stringbuilder and adds a new line
+
+            while ( (content = bufferedReader.readLine()) != null) {
+                stringBuilder.append(content + System.getProperty("line.separator"));
             }
+
+            //Close all IO parts
+
             fileInputStream.close();
-            line = stringBuilder.toString();
+            inputStreamReader.close();
             bufferedReader.close();
+
+            //Stores the whole new Stringbuilder content in a String
+            content = stringBuilder.toString();
+
         } catch(FileNotFoundException ex) {
+            content = "File not Found - " + ex.getMessage();
             Log.d(TAG, ex.getMessage());
+            return content;
         } catch (IOException ex ) {
+            content = "IO exception - " + ex.getMessage();
             Log.d(TAG, ex.getMessage());
+            return content;
         }
-        return line;
+        return content;
     }
 
     public static String investInput (String input) {
 
         if (!input.contains("<CscFeature_SystemUI_ConfigOverrideDataIcon>")) {
-            //Add parameter
+            //Add parameter xml code
             try {
                 Process su, mount;
-                File file = new File(path, filename);
 
                 su = Runtime.getRuntime().exec("su");
+                //Trying to get RW Status for root folders
                 mount = Runtime.getRuntime().exec("mount -o rw,remount /system");
-                FileOutputStream fileOutputStream = new FileOutputStream(file, true);
-                fileOutputStream.write(("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>" + System.getProperty("line.separator")).getBytes());
 
+
+                FileOutputStream fileOutputStream = new FileOutputStream(xmlText, true);        //Crashes atm --> Permission denied
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                outputStreamWriter.append(("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>" + System.getProperty("line.separator")));
+
+
+                outputStreamWriter.close();
+                fileOutputStream.flush();
+                fileOutputStream.close();
+
+                //fileOutputStream.write(("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>" + System.getProperty("line.separator")).getBytes());
+                //When my code is added, the icon will be 4G
                 return "4G";
             }catch (FileNotFoundException ex) {
                 Log.d(TAG, ex.getMessage());
--- app/src/main/java/com/android/misterevo/datachanger/MainActivity.java
+++ app/src/main/java/com/android/misterevo/datachanger/MainActivity.java
@@ -1,12 +1,12 @@
 package com.android.misterevo.datachanger;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     //Getting interface
     Button btnWrite, btnRead;
     TextView Output;
-    EditText Input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +21,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Connect elements
-        btnWrite = (Button) findViewById(R.id.btnchangeData);
-        btnRead = (Button) findViewById(R.id.readFile);
-        Output = (TextView) findViewById(R.id.textViewOutput);
-        Input = (EditText) findViewById(R.id.editTextInput) ;
+        btnWrite = (Button) findViewById(R.id.btnSaveFile);
+        btnRead = (Button) findViewById(R.id.btnReadFile);
+        Output = (TextView) findViewById(R.id.tvCSC);
 
-        //Load file
-        Output.setText(FileHelper.investInput(FileHelper.ReadFile(MainActivity.this)));
+        //Read OnClickListener
 
         btnRead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Output.setText(FileHelper.investInput(FileHelper.ReadFile(MainActivity.this)));
+                //Reading the content of the XMLFile
+                Output.setText(FileHelper.readFile());
             }
         });
 
         btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*if(FileHelper.saveToFile(Input.getText().toString())) {
-                    Toast.makeText(MainActivity.this, "Saved to file", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(MainActivity.this,"Error save file!!!",Toast.LENGTH_SHORT).show();
-                }*/
+                String xmlResult = FileHelper.readFile();
+                String saved = FileHelper.investInput(xmlResult);
+
+                Toast.makeText(MainActivity.this, saved, Toast.LENGTH_SHORT).show();
+
+
             }
         });
+
     }
 
 
