9cc6522e6c81b5fe05020e1c3948c8bd2fdb49d9
MisterEvo <hanscop@hotmail.de>
2017-02-20 17:47:08 +0100

    Optimized code Reduce SU Requests - less Toasts Added reboot alert window

--- app/src/main/java/com/android/misterevo/datachanger/FileHelper.java
+++ app/src/main/java/com/android/misterevo/datachanger/FileHelper.java
@@ -1,8 +1,8 @@
 package com.android.misterevo.datachanger;
 
 
-
 import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -14,84 +14,61 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 
-/**
- * Created by Lars on 15.02.2017.
- */
-
 public class FileHelper {
 
-    final static String TAG = FileHelper.class.getName();
+    private final static String TAG = FileHelper.class.getName();
+    Process p;
 
 
-    public static void copyFileToTemp(String cmd) {
+    public static void copyFileToTemp(String cmd, Process p) {
         try {
-            Process p = Runtime.getRuntime().exec("su");
             DataOutputStream outs = new DataOutputStream(p.getOutputStream());
             outs.writeBytes(cmd);
             outs.flush();
-        }catch (IOException ex) {
-           ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
-    public static void copyFileToRoot(String cmd) {
+    public static void copyFileToRoot(String cmd, Process p) {
         try {
-            Process p = Runtime.getRuntime().exec("su");
             DataOutputStream outs = new DataOutputStream(p.getOutputStream());
-            //Mount System to Read-Write
             outs.writeBytes("mount -o rw,remount /system\n");
-            //Copying the file from temp to root folder
             outs.writeBytes(cmd);
-            //Mounting back to Read-Only
             outs.writeBytes("mount -o ro,remount /system\n");
             outs.flush();
-        }catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
 
-    public static String readFile (File inputFile) {
+    public static String readFile(File inputFile) {
         String content;
         try {
-            //Acquire root-access
-            Process p;
-            p = Runtime.getRuntime().exec("su");
-
-            //FileInputStream loads the xmlText- File
-            //InputStreamReader reads the bytes coming from the FIS
-            //BufferedReader is converting to text?? -- Needs confirmation
-            //StringBuilder puts the single lines to one whole String together
 
             FileInputStream fileInputStream = new FileInputStream(inputFile);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder stringBuilder = new StringBuilder();
 
-            //Checking if there is a line to read
-            //Stringbuilder adds the line to the Stringbuilder and adds a new line
 
-            while ( (content = bufferedReader.readLine()) != null) {
+            while ((content = bufferedReader.readLine()) != null) {
                 stringBuilder.append(content + System.getProperty("line.separator"));
             }
-
-            //Close all IO parts
 
             fileInputStream.close();
             inputStreamReader.close();
             bufferedReader.close();
 
-            //Stores the whole new Stringbuilder content in a String
             content = stringBuilder.toString();
 
 
-
-
-        } catch(FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             content = "File not Found - " + ex.getMessage();
             Log.d(TAG, ex.getMessage());
             return content;
-        } catch (IOException ex ) {
+        } catch (IOException ex) {
             content = "IO exception - " + ex.getMessage();
             Log.d(TAG, ex.getMessage());
             return content;
@@ -99,19 +76,19 @@ public class FileHelper {
         return content;
     }
 
-    public static String investInput (String input, File tempFile) {
+    public static void investInput(String input, File tempFile) {
 
-        if (!input.contains("<CscFeature_SystemUI_ConfigOverrideDataIcon>")) {
-            //Add parameter xml code
+        if (input.contains("<CscFeature_SystemUI_ConfigOverrideDataIcon>LTE</CscFeature_SystemUI_ConfigOverrideDataIcon>")) {
+            return;
+        } else if (input.contains("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>")) {
+            return;
+        } else {
             try {
-                //Removing empty lines
                 input = input.replaceAll("(?m)^[ \t]*\r?\n", "");
                 StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder.append(input);
 
-
-                //Place code right before the last two tags
-                stringBuilder.insert(input.length()-39, "<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>\n");
+                stringBuilder.insert(input.length() - 39, "<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>\n");
 
                 FileOutputStream fileOutputStream = new FileOutputStream(tempFile, false);
                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
@@ -122,26 +99,17 @@ public class FileHelper {
                 fileOutputStream.flush();
                 fileOutputStream.close();
 
-                //When my code is added, the icon will be 4G
-                return "4G";
-            }catch (FileNotFoundException ex) {
+                return;
+            } catch (FileNotFoundException ex) {
                 Log.d(TAG, ex.getMessage());
-                return ex.getMessage();
-            }catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
                 Log.d(TAG, ex.getMessage());
-            }
-
-        } else {
-            //Read its value
-            if(input.contains("<CscFeature_SystemUI_ConfigOverrideDataIcon>LTE</CscFeature_SystemUI_ConfigOverrideDataIcon>")) {
-                return "LTE";
-            }
-            else if (input.contains("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>")) {
-                return "4G";
+                ex.printStackTrace();
             }
 
         }
-        return null;
+
     }
 
     public static boolean Toggled(String input) {
@@ -153,30 +121,29 @@ public class FileHelper {
         return false;
     }
 
-    public static String saveFile (String input,File tempFile) {
+    public static void saveFile(String input, File tempFile) {
         try {
+
             //Removing empty lines
             input = input.replaceAll("(?m)^[ \t]*\r?\n", "");
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(input);
 
 
             FileOutputStream fileOutputStream = new FileOutputStream(tempFile, false);
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-            outputStreamWriter.append(stringBuilder.toString());
+            outputStreamWriter.append(input);
 
 
             outputStreamWriter.close();
             fileOutputStream.flush();
             fileOutputStream.close();
 
-            return "File Saved";
-        }catch (FileNotFoundException ex) {
+            return;
+        } catch (FileNotFoundException ex) {
             Log.d(TAG, ex.getMessage());
-            return ex.getMessage();
-        }catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
             Log.d(TAG, ex.getMessage());
-            return ex.getMessage();
+            ex.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/android/misterevo/datachanger/MainActivity.java
+++ app/src/main/java/com/android/misterevo/datachanger/MainActivity.java
@@ -2,7 +2,10 @@ package com.android.misterevo.datachanger;
 
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -15,20 +18,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
 
     //Getting interface
-    TextView Output;
-    Switch swtData;
+    private TextView Output;
+    private Switch swtData;
     private static final int PERM_READ_EXT = 111;
     private static final int PERM_WRITE_EXT = 222;
-    private final String TEMP_FILE_NAME = "other_temp.xml";
-    String cmd;
-    String Result;
-    File tempFile = new File("/sdcard/download/" + TEMP_FILE_NAME);
+    private final String TEMP_FILE_NAME = ".other_temp.xml";
+    private final String cmdRT = "cat /system/csc/others.xml > " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + TEMP_FILE_NAME + "\n";
+    private final String cmdTR = "cat " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + TEMP_FILE_NAME + " > /system/csc/others.xml\n";
+    private String result;
+    private final File tempFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + TEMP_FILE_NAME);
+    Process p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,29 +48,34 @@ public class MainActivity extends AppCompatActivity {
         final int permissionsCheckRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
         final int permissionsCheckWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+        try {
+            p = Runtime.getRuntime().exec("su");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        //Loading the original xml into temp
-        cmd = "cat /system/csc/others.xml > /sdcard/download/" + TEMP_FILE_NAME + "\n";
 
         //Checking ReadStoragePermission
-        if(permissionsCheckRead != PackageManager.PERMISSION_GRANTED) {
+        if (permissionsCheckRead != PackageManager.PERMISSION_GRANTED) {
             //No access --> OpenRequestDialog
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERM_READ_EXT);
 
         } else {
             //Permissions is ok
 
-            //Copying File from Root to temp
-            FileHelper.copyFileToTemp(cmd);
+            FileHelper.copyFileToTemp(cmdRT, p);
             Runnable r = new Runnable() {
                 @Override
                 public void run() {
-                    Output.setText(FileHelper.readFile(tempFile));
+                    result = FileHelper.readFile(tempFile);
+                    FileHelper.investInput(result, tempFile);
+                    Output.setText(result);
+                    FileHelper.copyFileToRoot(cmdTR, p);
                     //Check if LTE is toggled
                     if (FileHelper.Toggled(Output.getText().toString())) {
                         swtData.setChecked(true);
 
-                    }else {
+                    } else {
                         swtData.setChecked(false);
                     }
                 }
@@ -77,25 +87,71 @@ public class MainActivity extends AppCompatActivity {
         swtData.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    Result = FileHelper.readFile(tempFile);
-                    Result = Result.replace("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>", "<CscFeature_SystemUI_ConfigOverrideDataIcon>LTE</CscFeature_SystemUI_ConfigOverrideDataIcon>");
-                    FileHelper.saveFile(Result, tempFile);
-                    cmd = "cat /sdcard/download/" + TEMP_FILE_NAME+ " > /system/csc/others.xml\n";
-                    FileHelper.copyFileToRoot(cmd);
-                    Toast.makeText(MainActivity.this, "Changed to 4G", Toast.LENGTH_SHORT);
-                }else {
-                    Result = FileHelper.readFile(tempFile);
-                    Result = Result.replace("<CscFeature_SystemUI_ConfigOverrideDataIcon>LTE</CscFeature_SystemUI_ConfigOverrideDataIcon>", "<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>");
-                    FileHelper.saveFile(Result, tempFile);
-                    cmd = "cat /sdcard/download/" + TEMP_FILE_NAME+ " > /system/csc/others.xml\n";
-                    FileHelper.copyFileToRoot(cmd);
-                    Toast.makeText(MainActivity.this, "Changed to LTE", Toast.LENGTH_SHORT);
+                if (permissionsCheckWrite != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERM_WRITE_EXT);
+                } else {
+                    if (isChecked) {
+                        result = FileHelper.readFile(tempFile);
+                        result = result.replace("<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>", "<CscFeature_SystemUI_ConfigOverrideDataIcon>LTE</CscFeature_SystemUI_ConfigOverrideDataIcon>");
+                        FileHelper.saveFile(result, tempFile);
+                        FileHelper.copyFileToRoot(cmdTR, p);
+                        Toast.makeText(MainActivity.this, "Changed to LTE", Toast.LENGTH_SHORT).show();
+                        Output.setText(result);
+
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Reboot phone")
+                                .setMessage("You need to reboot your device to apply changes.\n Do you want to reboot?")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        try {
+                                            p = Runtime.getRuntime().exec("reboot");
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                                .show();
+
+
+                    } else {
+                        result = FileHelper.readFile(tempFile);
+                        result = result.replace("<CscFeature_SystemUI_ConfigOverrideDataIcon>LTE</CscFeature_SystemUI_ConfigOverrideDataIcon>", "<CscFeature_SystemUI_ConfigOverrideDataIcon>4G</CscFeature_SystemUI_ConfigOverrideDataIcon>");
+                        FileHelper.saveFile(result, tempFile);
+                        FileHelper.copyFileToRoot(cmdTR, p);
+                        Toast.makeText(MainActivity.this, "Changed to 4G", Toast.LENGTH_SHORT).show();
+                        Output.setText(result);
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Reboot phone")
+                                .setMessage("You need to reboot your device to apply changes.\n Do you want to reboot?")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        try {
+                                            p = Runtime.getRuntime().exec("reboot");
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                                .show();
+
+                    }
                 }
             }
         });
-
-
 
 
     }
@@ -105,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case PERM_READ_EXT: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    FileHelper.copyFileToTemp(cmd);
+                    FileHelper.copyFileToTemp(cmdRT, p);
                     Runnable r = new Runnable() {
                         @Override
                         public void run() {
@@ -114,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
                             if (FileHelper.Toggled(Output.getText().toString())) {
                                 swtData.setChecked(true);
 
-                            }else {
+                            } else {
                                 swtData.setChecked(false);
                             }
                         }
@@ -129,10 +185,7 @@ public class MainActivity extends AppCompatActivity {
             }
             case PERM_WRITE_EXT: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                   // String xmlResult = FileHelper.readFile();
-                   // String saved = FileHelper.investInput(xmlResult);
-
-                  //  Toast.makeText(MainActivity.this, saved, Toast.LENGTH_SHORT).show();
+                    swtData.setChecked(swtData.isChecked());
                 } else {
                     Toast.makeText(MainActivity.this, "No Write Perm granted", Toast.LENGTH_SHORT).show();
                 }
